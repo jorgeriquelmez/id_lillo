5fa0b951ab24e20495adf545e3f769384293aaa5
jorge riquelme <jorge.riquelme.z@gmail.com>
2024-12-27 17:36:29 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// Este archivo se puede usar para añadir interactividad, como un formulario de contacto
+// o animaciones. Por ahora, lo dejamos vacío, pero es importante incluirlo.
+console.log("¡JavaScript funcionando!");
